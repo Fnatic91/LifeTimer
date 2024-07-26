3cec90f299699ae95a47e2ebba21a6d4a58f0a30
Fnatic91 <holikri@gmail.com>
2024-07-26 10:00:31 +0200

    Add files via upload

---
+++ precache-manifest.175f212bcf66d747e6950d55527b61d6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493b29205f9053e5f01c9f2b5d7f26ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d69aa24b91b5352cdce3",
+    "url": "/static/js/2.57b02d59.chunk.js"
+  },
+  {
+    "revision": "9db722aff971f3bc2e7e",
+    "url": "/static/js/main.a997bf8b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
