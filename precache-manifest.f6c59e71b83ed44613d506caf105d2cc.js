6c6b31e9826d9859d80edd3a4c0c30c0b42781e8
Fnatic91 <holikri@gmail.com>
2024-07-26 18:00:35 +0200

    Add files via upload

---
+++ precache-manifest.f6c59e71b83ed44613d506caf105d2cc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dca3afe0d7cbf070dc5f5fd3a846d7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d69aa24b91b5352cdce3",
+    "url": "/static/js/2.57b02d59.chunk.js"
+  },
+  {
+    "revision": "93a5ddbee147c551cd96",
+    "url": "/static/js/main.4401cb38.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66031e2b057036a64ff7e64236a1b4f7.js"
+  "/precache-manifest.f6c59e71b83ed44613d506caf105d2cc.js"
 );
 
 self.addEventListener('message', (event) => {
