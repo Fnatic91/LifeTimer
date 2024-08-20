e3daecca9e669d0bb597232a459c9b8b9b183f1f
Fnatic91 <holikri@gmail.com>
2024-08-20 13:57:56 +0200

    Add files via upload

---
+++ precache-manifest.f1dae1493f0bcdcb9b007232b6c43a6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b2bdaafeeda49b1f8aef6e178d9f83",
+    "url": "/LifeTimer/index.html"
+  },
+  {
+    "revision": "9ea58a237b78a89f74aa",
+    "url": "/LifeTimer/static/css/main.3c84736f.chunk.css"
+  },
+  {
+    "revision": "c0ee305eb3145ecf5a2b",
+    "url": "/LifeTimer/static/js/2.07b41d29.chunk.js"
+  },
+  {
+    "revision": "9ea58a237b78a89f74aa",
+    "url": "/LifeTimer/static/js/main.0c3d2598.chunk.js"
+  },
+  {
+    "revision": "dcbf71cf576fe6f7748d",
+    "url": "/LifeTimer/static/js/runtime~main.e4e854b7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.477156bf8762cd9b80fe053168fc071c.js"
+  "/LifeTimer/precache-manifest.f1dae1493f0bcdcb9b007232b6c43a6f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/LifeTimer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
